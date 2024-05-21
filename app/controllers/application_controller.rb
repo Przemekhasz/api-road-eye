0412f1f16e83e26f377cd304f70d66d2e940da82
Przemekhash <tarapackiprzemek45@gmail.com>
2024-05-21 21:41:46 +0200

    Add user registration, login, JWT authentication, and refactor code to use service and repository patterns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JWT.decode(header, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+      @current_user = User.find(@decoded[0]['user_id'])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
